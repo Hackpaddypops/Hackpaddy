510dada52893b1aa6bfa28499c011a11a7c0ab41
Hackpaddypops <padmesh.bhalla.tech@gmail.com>
2026-06-18 16:39:59 +0530

    Basic Maths Practice Complete

--- Core Java/BasicMaths.java
+++ Core Java/BasicMaths.java
@@ -7,12 +7,15 @@ public class BasicMaths {
         Scanner in = new Scanner(System.in);
         // System.out.println("Enter number : ");
         // int n = in.nextInt();
-        int n = 4624;
+        int n = 36;
 
         countDigits(n);
         reverseNum(n);
         palindrome(n);
         gcd(12, 15);
+        armsStrongNum(153);
+        printAllDivisors(n);
+        primeNumber(n);
     }
 
     private static void countDigits(int n) {
@@ -51,21 +54,64 @@ public class BasicMaths {
 
     private static void gcd(int num1, int num2) {
         int gcd = 0;
-        int min = 0;
-        if (num1 > num2) {
-            min = num2;
-        } else {
-            min = num1;
-        }
-        //Better approach
-        for (int i = min; i >= 1; i--) {
-            if (num1 % i == 0 && num2 % i == 0) {
-                gcd = i;
-                break;
+
+        //Best approach
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) {
+                num1 %= num2;
+            } else {
+                num2 %= num1;
             }
         }
-        System.out.println("GCD of " + num1 + " and " + num2 + " is " + gcd);
+        if(num1 == 0){
+            gcd = num2;
+        } else
+            gcd = num1;
+        System.out.println("GCD is " + gcd);
 
+    }
+
+    private static void armsStrongNum(int i) {
+        int digitCount = (int) Math.log10(i) + 1;
+        int armNum = 0;
+        int origNum = i;
+        while(i>0){
+            armNum += Math.pow((i%10), digitCount);
+            i/=10;
+        }
+        if(origNum == armNum){
+            System.out.println(origNum + " is an armstrong number.");
+        } else
+            System.out.println(origNum + " is not an armstrong number.");
+
+    }
+
+    private static void printAllDivisors(int n) {
+        System.out.print(n + " is divisible by 1");
+        for(int i = 2; i*i<n;i++ ){
+            if(n%i==0){
+                System.out.print(","+i);
+                if(i != (n/i)){
+                    System.out.print(","+n/i);
+                }
+            }
+        }
+        System.out.println(","+n+".");
+    }
+
+    private static void primeNumber(int n) {
+        if(n<4){
+            System.out.println(n + " is a prime number");
+        } else{
+            for(int i = 2; i*i<n;i++ ){
+            if(n%i==0){
+                System.out.println(n + " is not a prime number");
+                return;
+            }
+        }
+        }
+        System.out.println(n + " is a prime number");
+        
     }
 
 }
